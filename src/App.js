5f4f05e6fd3315e8d2ae463d19f8d9fb13094801
Huaxun Chen <120090440@link.cuhk.edu.cn>
2024-07-10 03:09:51 +0800

    home page draft

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import ExpandingCardsPage from "./pages/ExpandingCardsPage";
 import RandomChoicePickerPage from "./pages/RandomChoicePickerPage";
 import AnimatedNavigationPage from "./pages/AnimatedNavigationPage";
@@ -13,8 +13,10 @@ import DragNDropPage from "./pages/DragNDropPage";
 import DrawingAppPage from "./pages/DrawingAppPage";
 
 function App() {
+  const location = useLocation();
   return (
-    <BrowserRouter>
+    <div>
+      {location.pathname === "/" && <Nav />}
       <Routes>
         <Route path="/expanding-cards" element={<ExpandingCardsPage />} />
         <Route
@@ -43,8 +45,107 @@ function App() {
         <Route path="/drag-N-drop" element={<DragNDropPage />}></Route>
         <Route path="/drawing-app" element={<DrawingAppPage />}></Route>
       </Routes>
+    </div>
+  );
+}
+
+const Nav = () => {
+  return (
+    <div>
+      <h1>Only For Convenient</h1>
+      <nav>
+        <ul>
+          <li>
+            <a
+              href="/expanding-cards"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Expanding Cards
+            </a>
+          </li>
+          <li>
+            <a
+              href="/random-choice-picker"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Random Choice Picker
+            </a>
+          </li>
+          <li>
+            <a
+              href="/animated-navigation"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Animated Navigation
+            </a>
+          </li>
+          <li>
+            <a
+              href="/incrementing-counter"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Incrementing Counter
+            </a>
+          </li>
+          <li>
+            <a href="/drink-water" target="_blank" rel="noopener noreferrer">
+              Drink Water
+            </a>
+          </li>
+          <li>
+            <a href="/movie-app" target="_blank" rel="noopener noreferrer">
+              Movie App
+            </a>
+          </li>
+          <li>
+            <a
+              href="/background-slider"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Background Slider
+            </a>
+          </li>
+          <li>
+            <a href="/theme-clock" target="_blank" rel="noopener noreferrer">
+              Theme Clock
+            </a>
+          </li>
+          <li>
+            <a
+              href="/button-ripple-effect"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Button Ripple Effect
+            </a>
+          </li>
+          <li>
+            <a href="/drag-N-drop" target="_blank" rel="noopener noreferrer">
+              Drag N Drop
+            </a>
+          </li>
+          <li>
+            <a href="/drawing-app" target="_blank" rel="noopener noreferrer">
+              Drawing App
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+function Root() {
+  return (
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
   );
 }
 
-export default App;
+export default Root;

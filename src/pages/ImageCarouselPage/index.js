798c960603da05923d1dd28bef89408d8ed1a265
Huaxun Chen <120090440@link.cuhk.edu.cn>
2024-07-29 02:58:34 +0800

    day 35

--- src/pages/ImageCarouselPage/index.js
+++ src/pages/ImageCarouselPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import styles from "./styles.module.css";
 
 const ImageCarousel = () => {
@@ -20,56 +20,61 @@ const ImageCarousel = () => {
     // slideRef.current.style.transform = `translateX(-${activeImage * 500}px)`;
   };
 
-  const handleClickNext = () => {
+  const handleClickNext = useCallback(() => {
     setActiveImage((prevImage) => (prevImage + 1) % images.length);
     // slideRef.current.style.transform = `translateX(-${activeImage * 500}px)`;
-  };
+  }, [images.length]);
 
   useEffect(() => {
     slideRef.current.style.left = `${500 * (images.length - 1)}px`;
   }, [images.length]);
 
   useEffect(() => {
+    const interval = setInterval(() => {
+      handleClickNext();
+    }, 3000);
+
     const updateImage = () => {
       slideRef.current.style.transform = `translateX(-${activeImage * 500}px)`;
     };
     updateImage();
-
     const handleResize = () => {
       updateImage();
     };
 
     window.addEventListener("resize", handleResize);
-
     return () => {
+      clearInterval(interval);
       window.removeEventListener("resize", handleResize);
     };
-  }, [activeImage]);
+  }, [activeImage, handleClickNext]);
 
   return (
     <div className={styles.body}>
-      <div className={styles.imageContainer} ref={slideRef}>
-        {images.map((image) => (
-          <div
-            key={image.id}
-            className={styles.image}
-            style={{ backgroundImage: `url${image.url}` }}
-          ></div>
-        ))}
-      </div>
-      <div className={styles.buttonContainer}>
-        <button
-          className={`${styles.button} ${styles.prev}`}
-          onClick={handleClickPrev}
-        >
-          Prev
-        </button>
-        <button
-          className={`${styles.button} ${styles.next}`}
-          onClick={handleClickNext}
-        >
-          Next
-        </button>
+      <div className={styles.carousel}>
+        <div className={styles.imageContainer} ref={slideRef}>
+          {images.map((image) => (
+            <div
+              key={image.id}
+              className={styles.image}
+              style={{ backgroundImage: `url(${image.url})` }}
+            ></div>
+          ))}
+        </div>
+        <div className={styles.buttonContainer}>
+          <button
+            className={`${styles.button} ${styles.prev}`}
+            onClick={handleClickPrev}
+          >
+            Prev
+          </button>
+          <button
+            className={`${styles.button} ${styles.next}`}
+            onClick={handleClickNext}
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );

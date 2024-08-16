f9344d79d503ba8f43770293988d1f370d58137f
Huaxun Chen <120090440@link.cuhk.edu.cn>
2024-08-17 01:06:31 +0800

    day 43

--- src/pages/FeedbackDesignPage/index.js
+++ src/pages/FeedbackDesignPage/index.js
@@ -1,11 +1,9 @@
-import React, { useState, useRef } from "react";
-import { CSSTransition } from "react-transition-group";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 
 const FeedbackDesign = () => {
   const [activeNum, setActiveNum] = useState(3);
   const [finish, setFinish] = useState(false);
-  const nodeRef = useRef(null);
 
   const rating = [
     {
@@ -25,48 +23,59 @@ const FeedbackDesign = () => {
     },
   ];
 
+  const handleFinish = () => {
+    if (document.startViewTransition) {
+      document.startViewTransition(() => {
+        setFinish(true);
+      });
+    } else {
+      setFinish(true);
+    }
+  };
+
   return (
     <div className={styles.body}>
-      <div className={styles.panel}>
-        {finish && (
-          <>
-            <i className={`${styles.icon} fas fa-heart`}></i>
-            <strong>Thank You!</strong>
-            <br />
-            <strong>Feedback: {rating[activeNum - 1].text}</strong>
-            <br />
-            <p className={styles.end}>
-              We'll use your feedback to improve our customer support.
-            </p>
-          </>
-        )}
-        {!finish && (
-          <div ref={nodeRef}>
-            <strong>
-              How satisfied are you with our <br /> customer support
-              performance?
-            </strong>
-            <div className={styles.container}>
-              {rating.map((item) => (
-                <div
-                  className={`${styles.item} ${
-                    activeNum === item.num ? styles.active : ""
-                  }`}
-                  key={item.num}
-                  onClick={() => setActiveNum(item.num)}
-                >
-                  <img src={item.src} alt={item.text} />
-                  <small>{item.text}</small>
-                </div>
-              ))}
-            </div>
-            <button className={styles.btn} onClick={() => setFinish(true)}>
-              Send Review
-            </button>
+      {/* <div className={styles.panel}> */}
+      {finish ? (
+        <div className={styles.panel} style={{ viewTransitionName: "fade-in" }}>
+          <i className={`${styles.icon} fas fa-heart`}></i>
+          <strong>Thank You!</strong>
+          <br />
+          <strong>Feedback: {rating[activeNum - 1].text}</strong>
+          <br />
+          <p className={styles.end}>
+            We'll use your feedback to improve our customer support.
+          </p>
+        </div>
+      ) : (
+        <div
+          className={styles.panel}
+          style={{ viewTransitionName: "fade-out" }}
+        >
+          <strong>
+            How satisfied are you with our <br /> customer support performance?
+          </strong>
+          <div className={styles.container}>
+            {rating.map((item) => (
+              <div
+                className={`${styles.item} ${
+                  activeNum === item.num ? styles.active : ""
+                }`}
+                key={item.num}
+                onClick={() => setActiveNum(item.num)}
+              >
+                <img src={item.src} alt={item.text} />
+                <small>{item.text}</small>
+              </div>
+            ))}
           </div>
-        )}
-      </div>
+          <button className={styles.btn} onClick={handleFinish}>
+            Send Review
+          </button>
+        </div>
+      )}
     </div>
+    // </div>
   );
 };
 

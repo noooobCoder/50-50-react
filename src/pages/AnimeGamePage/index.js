c12b9b7b5be998eed1420b533dea19ca71d9a2be
Huaxun Chen <120090440@link.cuhk.edu.cn>
2024-09-04 14:52:10 +0800

    main page style change

---
+++ src/pages/AnimeGamePage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const AnimeGame = () => {
+  return (
+    <div className={styles.body}>
+      <div></div>
+    </div>
+  );
+};
+
+export default AnimeGame;
